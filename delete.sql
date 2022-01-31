e9815b891e4d86125dac3668b24d8c5b0f494be5
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-31 15:56:49 -0600

    DeleteComplete

--- delete.sql
+++ delete.sql
@@ -1,10 +1,27 @@
--- INSERT INTO animals ( name, type, age ) 
--- VALUES ('Leo', 'lion', 12),
--- ('Jerry', 'mouse', 4),
--- ('Marty', 'zebra', 10),
--- ('Gloria', 'hippo', 8),
--- ('Alex', 'lion', 9),
--- ('Melman', 'giraffe', 15),
--- ('Nala', 'lion', 2),
--- ('Marie', 'cat', 1),
--- ('Flounder', 'fish', 8);
+create table animals (
+    id serial primary key,
+    name varchar(30),
+    type varchar(30),
+    age int
+);
+
+
+INSERT INTO animals ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+
+delete from animals
+where type = 'lion';
+
+delete from animals
+where name like 'M%';
+
+delete from animals
+where age < 9;
